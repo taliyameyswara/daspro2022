0eeba780abf3f4f53da15e1bb4b4b02c816c6d71
taliyameyswara <taliyameyswara@gmail.com>
2022-10-23 20:36:11 +0700

    update

--- minggu7/no4 but make it 3digits.cpp
+++ minggu7/no4 but make it 3digits.cpp
@@ -7,18 +7,8 @@ int main()
     cout << "Taliya Meyswara (A11.2022.14163)" << endl;
     cout << "=====================================" << endl;
     int num,first,sec,last,rev;
-    cout << "Input 3 digit number = " ; cin >> num;
-    if (num>=100 && num<=999){
-        cout << "Your number is positive" << endl;
-        if (num%2==0){
-            cout << "Your number is even" << endl;
-        }
-        else {
-            cout << "Your number is odd" << endl;
-        }
-        //145/100 = 1
-        //145%10= (145-1)%100/10 = 4
-        //145%10 = 5
+        cout << "input 3 digits of numbers = " ; cin >> num;
+        if (num>=100 && num<=999){
         first = num/100;
         sec = ((num-1)%100)/10;
         last = num%10;
@@ -44,11 +34,11 @@ int main()
             cout << last << " is the largest number" << endl;
         }
 
-        rev=(last*100)+(sec*10)+first;
-        //reverse number
-        cout << "Reversed number = " << rev << endl;
-
-    }
+        //same
+        if (first==sec || first==last || sec==first || sec==last || last==first || last==sec){
+            cout << "there are the same numbers" << endl;
+        }
+        }
     else {
         cout << "Your number less or more than 3 digit" << endl;
     }
